f7c8bb09625265c2ad108f7e076ba0ba71315943
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-21 09:05:16 -0400

    first commit

---
+++ FirePhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirePhotos
+//
+//  Created by DetroitLabs on 6/21/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirePhotos/FirePhotoCVC.h
@@ -0,0 +1,13 @@
+//
+//  FirePhotoCVC.h
+//  FirePhotos
+//
+//  Created by DetroitLabs on 6/21/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirePhotoCVC : UICollectionViewController
+
+@end
