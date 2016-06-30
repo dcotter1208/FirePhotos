cdc3fc2a04323821323c7ed8e06352f2d63ef548
dcotter1208 <donovan.cotter@yahoo.com>
2016-06-30 11:10:44 -0400

    reducing image size before sending to Firebase storage to speed up download process

--- FirePhotos/PhotosTVC.m
+++ FirePhotos/PhotosTVC.m
@@ -10,12 +10,14 @@
 #import "Photo.h"
 #import "PhotoTableViewCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "SelectedImageViewController.h"
 @import FirebaseDatabase;
 
 @interface PhotosTVC ()
 
 @property(nonatomic, strong) NSMutableArray *photoArray;
 @property(nonatomic, strong) Photo *photo;
+@property(nonatomic, strong) UIImage *selectedPhoto;
 @end
 
 @implementation PhotosTVC
@@ -59,54 +61,21 @@
     
     [cell.cellImageView setImageWithURL:[NSURL URLWithString:photo.downloadURL] placeholderImage:[UIImage imageNamed:@"placeholder"]];
     
-//    cell.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:photo.downloadURL]]];
-    
     return cell;
 }
 
+#pragma mark - PrepareForSegue
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    SelectedImageViewController *destVC = [segue destinationViewController];
+    
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    
+    Photo *selectedPhotoToPass = [_photoArray objectAtIndex:indexPath.row];
+    destVC.selectedImage = selectedPhotoToPass;
+        
 }
-*/
+
 
 @end
